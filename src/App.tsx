06f068bc395b1455c0c34bc4ece584f9c03f313c
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2022-10-05 04:59:02 +0600

    fix: lint app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.scss'
+import './App.scss';
 
 const App = () => (
-  <h1>Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS</h1>
+  <h1>
+    Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello
+    LMS
+  </h1>
 );
 
 export default App;
