7a0382979589a33ef61461c6be44a418eb49e683
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2022-10-05 04:23:52 +0600

    feat: pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import './App.scss';
+import './App.scss'
 
 const App = () => (
-  <h1>
-    Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello
-    LMS
-  </h1>
+  <h1>Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS</h1>
 );
 
 export default App;
