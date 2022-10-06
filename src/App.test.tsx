04fd02240f32b385f3fa19e03fde0b5ae88ce390
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2022-10-07 04:43:02 +0600

    feat: setup tests

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './App';
+import { render } from './test-utils';
+
+describe('App tests', () => {
+  it('should contains the heading 1', () => {
+    const component = render(<App />);
+    expect(component.getByText('Hello LMS')).toBeInTheDocument();
+  });
+});
