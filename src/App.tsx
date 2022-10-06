6d66aa0d137655ecfb78ef870159709c8159bd14
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2022-10-06 23:08:35 +0600

    fix: husky lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.scss';
 
-const App = () => (
-  <h1>
-    Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello LMS Hello
-    LMS
-  </h1>
-);
+const App = () => <h1>Hello LMS</h1>;
 
 export default App;
