d5204d13e01eb9a7179d19aa99bce86c2286a362
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-10-31 20:15:22 +0600

    feat:finished feat/studentPageSuperAdmin

--- src/constants/StudentPage.ts
+++ src/constants/StudentPage.ts
@@ -1,26 +1,26 @@
-export const inputsData: { placeholder: string; }[] = [
-    {
-        placeholder: 'Name'
-    },
-    {
-        placeholder: 'Number'
-    },
-    {
-        placeholder: 'Age'
-    },
-    {
-        placeholder: 'Start Date'
-    },
-    {
-        placeholder: 'Auto Generated Password'
-    },
-    {
-        placeholder: 'Surname'
-    },
-    {
-        placeholder: 'Mail'
-    },
-    {
-        placeholder: 'Price'
-    },
-]
+export const inputsData: { placeholder: string }[] = [
+  {
+    placeholder: 'Name',
+  },
+  {
+    placeholder: 'Number',
+  },
+  {
+    placeholder: 'Age',
+  },
+  {
+    placeholder: 'Start Date',
+  },
+  {
+    placeholder: 'Auto Generated Password',
+  },
+  {
+    placeholder: 'Surname',
+  },
+  {
+    placeholder: 'Mail',
+  },
+  {
+    placeholder: 'Price',
+  },
+];
--- src/pages/studentPage/StudentPage.tsx
+++ src/pages/studentPage/StudentPage.tsx
@@ -1,65 +1,79 @@
 import React from 'react';
-import useStyles from './StudentPage.style';
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useTranslation } from 'react-i18next';
 import { inputsData } from '../../constants/StudentPage';
+import useStyles from './StudentPage.style';
 
 const StudentPage: React.FC = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.container}>
-            <h1 className={classes.title}>Student Data</h1>
-            <div className={classes.inputsContainer}>
-                {
-                    inputsData.map((item) => (
-                        <TextField sx={{ width: '40%' }} id="outlined-basic" label={item.placeholder} />
-                    ))
-                }
-                <FormControl sx={{ m: 1, width: '35%' }}>
-                    <InputLabel id="demo-multiple-name-label">Status</InputLabel>
-                    <Select
-                        labelId="demo-multiple-name-label"
-                        id="demo-multiple-name"
-                    >
-                        <MenuItem>
-                            Admin
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
-            <div className={classes.btnsContainer}>
-                <Button
-                    variant="outlined"
-                    sx={{
-                        width: '15%',
-                        minHeight: '50px',
-                        marginRight: '5%',
-                        '@media(max-width: 425px)': {
-                            fontSize: '12px',
-                        },
-                    }}
-                >Save</Button>
-                <Button
-                    variant="outlined"
-                    startIcon={<DeleteIcon sx={{
-                        '@media(max-width: 580px)': {
-                            width: '12px',
-                        },
-                    }} />}
-                    sx={{
-                        width: '15%',
-                        minHeight: '50px',
-                        gap: '0',
-                        '@media(max-width: 580px)': {
-                            fontSize: '12px',
-                        },
-                    }}
-                >
-                    Delete
-                </Button>
-            </div>
-        </div>
-    );
+  const classes = useStyles();
+  const { t } = useTranslation();
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.title}>{t('StudentPage.title')}</h1>
+      <div className={classes.inputsContainer}>
+        {inputsData.map((item) => (
+          <TextField
+            sx={{ width: '40%' }}
+            id="outlined-basic"
+            label={item.placeholder}
+          />
+        ))}
+        <FormControl sx={{ m: 1, width: '35%' }}>
+          <InputLabel id="demo-multiple-name-label">
+            {t('StudentPage.status')}
+          </InputLabel>
+          <Select labelId="demo-multiple-name-label" id="demo-multiple-name">
+            <MenuItem />
+          </Select>
+        </FormControl>
+      </div>
+      <div className={classes.btnsContainer}>
+        <Button
+          variant="outlined"
+          sx={{
+            width: '15%',
+            minHeight: '50px',
+            marginRight: '5%',
+            '@media(max-width: 425px)': {
+              fontSize: '12px',
+            },
+          }}
+        >
+          {t('StudentPage.save')}
+        </Button>
+        <Button
+          variant="outlined"
+          startIcon={
+            <DeleteIcon
+              sx={{
+                '@media(max-width: 580px)': {
+                  width: '12px',
+                },
+              }}
+            />
+          }
+          sx={{
+            width: '15%',
+            minHeight: '50px',
+            gap: '0',
+            '@media(max-width: 580px)': {
+              fontSize: '12px',
+            },
+          }}
+        >
+          {t('StudentPage.delete')}
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
