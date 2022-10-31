57e5251acba47a76d38ef9b2db0e4eade9af0338
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-10-31 20:34:49 +0600

    fix:deleted comment in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PageContainer from './pageContainer/PageContainer';
-import StudentPage from './studentPage/StudentPage';
 
 const HomePage: React.FC = () => {
   const { t } = useTranslation();
   return (
     <div>
-      {/* <PageContainer name="User" puth="*" btnText="Click here">
+      <PageContainer name="User" puth="*" btnText="Click here">
         <h1>{t('LMS')}</h1>
-      </PageContainer> */}
-      <StudentPage />
+      </PageContainer>
     </div>
   );
 };
