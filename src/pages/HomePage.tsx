f9c71b1e87ddf4da868983270a7a308aa701e00f
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-03 17:18:14 +0600

    fix: changed homePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import AdminMentorsPage from '../components/mentorsPageAdmin/AdminMentorsPage';
+import { useTranslation } from 'react-i18next';
 import PageContainer from '../components/PageContainer/PageContainer';
 
-const HomePage: React.FC = () => (
-  <div>
-    <PageContainer name="Mentors" puth="*" btnText="Add">
-      <AdminMentorsPage />
-    </PageContainer>
-  </div>
-);
+const HomePage: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <PageContainer name="User" puth="*" btnText="Click here">
+        <h1>{t('LMS')}</h1>
+      </PageContainer>
+    </div>
+  );
+};
 
 export default HomePage;
