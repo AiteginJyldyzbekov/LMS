fdfaca3282c40e7dcae5178bc49f5d3b5b081359
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-09 00:18:35 +0600

    feat:home works mentor

--- src/pages/homeWorksMentor/HomeWorksMentor.tsx
+++ src/pages/homeWorksMentor/HomeWorksMentor.tsx
@@ -1,36 +1,30 @@
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Grid,
-  Typography,
-} from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTranslation } from 'react-i18next';
+import HomeWorksSelect from '../../components/homeWorksMentorSelect/HomeWorksAccordion';
 
 const HomeWorksMentor: React.FC = () => {
   const { t } = useTranslation();
   const data = [
     {
       title: 'Home work 1',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin. Nullam massa odio, sollicitudin ut nibh sit amet, ullamcorper pretium lacus. Nullam rutrum mattis varius. Vestibulum eleifend feugiat enim eu mattis. Donec accumsan ipsum eu blandit vestibulum. Sed vel mi mattis, sodales leo at, pharetra mi. Quisque sit amet rutrum justo, at eleifend enim. Donec sodales tellus convallis enim ornare, a scelerisque nisl luctus. Sed semper suscipit mauris sit amet aliquam. Ut aliquam fermentum interdum. Morbi semper lectus sed tellus pellentesque, ac auctor sapien egestas. Praesent ut est vel mauris interdum interdum ac quis ipsum. Proin sit amet augue facilisis, convallis libero quis, ullamcorper orci. Nunc porta urna sit amet lorem placerat, eget accumsan magna auctor.',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
     },
     {
       title: 'Home work 2',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin. Nullam massa odio, sollicitudin ut nibh sit amet, ullamcorper pretium lacus. Nullam rutrum mattis varius. Vestibulum eleifend feugiat enim eu mattis. Donec accumsan ipsum eu blandit vestibulum. Sed vel mi mattis, sodales leo at, pharetra mi. Quisque sit amet rutrum justo, at eleifend enim. Donec sodales tellus convallis enim ornare, a scelerisque nisl luctus. Sed semper suscipit mauris sit amet aliquam. Ut aliquam fermentum interdum. Morbi semper lectus sed tellus pellentesque, ac auctor sapien egestas. Praesent ut est vel mauris interdum interdum ac quis ipsum. Proin sit amet augue facilisis, convallis libero quis, ullamcorper orci. Nunc porta urna sit amet lorem placerat, eget accumsan magna auctor.',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
     },
     {
       title: 'Home work 3',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin. Nullam massa odio, sollicitudin ut nibh sit amet, ullamcorper pretium lacus. Nullam rutrum mattis varius. Vestibulum eleifend feugiat enim eu mattis. Donec accumsan ipsum eu blandit vestibulum. Sed vel mi mattis, sodales leo at, pharetra mi. Quisque sit amet rutrum justo, at eleifend enim. Donec sodales tellus convallis enim ornare, a scelerisque nisl luctus. Sed semper suscipit mauris sit amet aliquam. Ut aliquam fermentum interdum. Morbi semper lectus sed tellus pellentesque, ac auctor sapien egestas. Praesent ut est vel mauris interdum interdum ac quis ipsum. Proin sit amet augue facilisis, convallis libero quis, ullamcorper orci. Nunc porta urna sit amet lorem placerat, eget accumsan magna auctor.',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
     },
     {
       title: 'Home work 4',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin. Nullam massa odio, sollicitudin ut nibh sit amet, ullamcorper pretium lacus. Nullam rutrum mattis varius. Vestibulum eleifend feugiat enim eu mattis. Donec accumsan ipsum eu blandit vestibulum. Sed vel mi mattis, sodales leo at, pharetra mi. Quisque sit amet rutrum justo, at eleifend enim. Donec sodales tellus convallis enim ornare, a scelerisque nisl luctus. Sed semper suscipit mauris sit amet aliquam. Ut aliquam fermentum interdum. Morbi semper lectus sed tellus pellentesque, ac auctor sapien egestas. Praesent ut est vel mauris interdum interdum ac quis ipsum. Proin sit amet augue facilisis, convallis libero quis, ullamcorper orci. Nunc porta urna sit amet lorem placerat, eget accumsan magna auctor.',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
     },
     {
       title: 'Home work 5',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin. Nullam massa odio, sollicitudin ut nibh sit amet, ullamcorper pretium lacus. Nullam rutrum mattis varius. Vestibulum eleifend feugiat enim eu mattis. Donec accumsan ipsum eu blandit vestibulum. Sed vel mi mattis, sodales leo at, pharetra mi. Quisque sit amet rutrum justo, at eleifend enim. Donec sodales tellus convallis enim ornare, a scelerisque nisl luctus. Sed semper suscipit mauris sit amet aliquam. Ut aliquam fermentum interdum. Morbi semper lectus sed tellus pellentesque, ac auctor sapien egestas. Praesent ut est vel mauris interdum interdum ac quis ipsum. Proin sit amet augue facilisis, convallis libero quis, ullamcorper orci. Nunc porta urna sit amet lorem placerat, eget accumsan magna auctor.',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
     },
   ];
   return (
@@ -66,22 +60,7 @@ const HomeWorksMentor: React.FC = () => {
         }}
       >
         {data.map((item) => (
-          <Accordion
-            sx={{
-              marginTop: '10px',
-            }}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>{item.title}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>{item.text}</Typography>
-            </AccordionDetails>
-          </Accordion>
+          <HomeWorksSelect title={item.title} text={item.text} />
         ))}
       </Grid>
     </div>
