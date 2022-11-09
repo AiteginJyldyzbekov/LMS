37df3ee25087ac90623ff082f4a77235769f44bb
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-09 17:11:24 +0600

    fix: fixed issues

--- src/pages/studyPlanMentor/StudyPlanMentor.tsx
+++ src/pages/studyPlanMentor/StudyPlanMentor.tsx
@@ -5,49 +5,49 @@ import AccordionMentor from '../../components/AccordionMentor/AccordionMentor';
 import MainPageContainer from '../mainPageContainer/MainPageContainer';
 
 const StudyPlanMentor: React.FC = () => {
-    const { t } = useTranslation();
-    const data = [
-        {
-            title: 'Study Plan 1',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-        },
-        {
-            title: 'Study Plan 2',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-        },
-        {
-            title: 'Study Plan 3',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-        },
-        {
-            title: 'Study Plan 4',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-        },
-        {
-            title: 'Study Plan 5',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-        },
-    ];
-    return (
-        <div>
-            <MainPageContainer>
-                <Typography
-                    textAlign="center"
-                    component="p"
-                    sx={{
-                        fontSize: '30px',
-                        fontWeight: '400',
-                        marginBottom: '20px',
-                    }}
-                >
-                    {t('StudyPlanMentor.title')}
-                </Typography>
-                {data.map((item) => (
-                    <AccordionMentor title={item.title} text={item.text} />
-                ))}
-            </MainPageContainer>
-        </div>
-    );
+  const { t } = useTranslation();
+  const data = [
+    {
+      title: 'Study Plan 1',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+    },
+    {
+      title: 'Study Plan 2',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+    },
+    {
+      title: 'Study Plan 3',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+    },
+    {
+      title: 'Study Plan 4',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+    },
+    {
+      title: 'Study Plan 5',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+    },
+  ];
+  return (
+    <div>
+      <MainPageContainer>
+        <Typography
+          textAlign="center"
+          component="p"
+          sx={{
+            fontSize: '30px',
+            fontWeight: '400',
+            marginBottom: '20px',
+          }}
+        >
+          {t('StudyPlanMentor.title')}
+        </Typography>
+        {data.map((item) => (
+          <AccordionMentor title={item.title} text={item.text} />
+        ))}
+      </MainPageContainer>
+    </div>
+  );
 };
 
 export default StudyPlanMentor;
