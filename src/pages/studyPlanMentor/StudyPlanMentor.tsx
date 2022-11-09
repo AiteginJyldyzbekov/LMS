94bc3462cc232606efcf8bc04c72d48dca0e8a44
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-09 16:54:09 +0600

    feat: take study plan to mainPageContainer

--- src/pages/studyPlanMentor/StudyPlanMentor.tsx
+++ src/pages/studyPlanMentor/StudyPlanMentor.tsx
@@ -2,69 +2,52 @@ import { Grid, Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import AccordionMentor from '../../components/AccordionMentor/AccordionMentor';
+import MainPageContainer from '../mainPageContainer/MainPageContainer';
 
 const StudyPlanMentor: React.FC = () => {
-  const { t } = useTranslation();
-  const data = [
-    {
-      title: 'Study Plan 1',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-    },
-    {
-      title: 'Study Plan 2',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-    },
-    {
-      title: 'Study Plan 3',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-    },
-    {
-      title: 'Study Plan 4',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-    },
-    {
-      title: 'Study Plan 5',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
-    },
-  ];
-  return (
-    <div>
-      <Typography
-        textAlign="center"
-        component="p"
-        sx={{
-          fontSize: '30px',
-          fontWeight: '400',
-          marginTop: '30px',
-        }}
-      >
-        {t('StudyPlanMentor.title')}
-      </Typography>
-      <Grid
-        sx={{
-          paddingRight: '60px',
-          paddingLeft: '60px',
-          marginTop: '20px',
-          '@media (max-width: 1200px)': {
-            paddingRight: '40px',
-            paddingLeft: '40px',
-          },
-          '@media (max-width: 768px)': {
-            paddingRight: '20px',
-            paddingLeft: '20px',
-          },
-          '@media (max-width: 375px)': {
-            paddingRight: '10px',
-            paddingLeft: '10px',
-          },
-        }}
-      >
-        {data.map((item) => (
-          <AccordionMentor title={item.title} text={item.text} />
-        ))}
-      </Grid>
-    </div>
-  );
+    const { t } = useTranslation();
+    const data = [
+        {
+            title: 'Study Plan 1',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+        },
+        {
+            title: 'Study Plan 2',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+        },
+        {
+            title: 'Study Plan 3',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+        },
+        {
+            title: 'Study Plan 4',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+        },
+        {
+            title: 'Study Plan 5',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod congue mi vitae sollicitudin.',
+        },
+    ];
+    return (
+        <div>
+            <MainPageContainer>
+                <Typography
+                    textAlign="center"
+                    component="p"
+                    sx={{
+                        fontSize: '30px',
+                        fontWeight: '400',
+                        marginTop: '30px',
+                    }}
+                >
+                    {t('StudyPlanMentor.title')}
+                </Typography>
+                {data.map((item) => (
+                    <AccordionMentor title={item.title} text={item.text} />
+                ))}
+            </MainPageContainer>
+        </div>
+    );
 };
 
 export default StudyPlanMentor;
