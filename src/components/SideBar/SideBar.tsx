4d61007c009369392258da38079cd7f48f914525
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-16 23:51:31 +0600

    fix: fixed text in 18n and role in sideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { useAppSelector } from '../../hooks/hook';
 import { admin, student, mentor } from '../../constants/sideBar';
 import RoleCard from './RoleCards';
 
 const SideBar: React.FC = () => {
-  const { t } = useTranslation();
   const { data } = useAppSelector((state) => state.auth);
   const role: string = data?.role || '';
 
-  if (role === t('SideBar.Admin')) {
+  if (role === 'admin') {
     return <RoleCard role={admin} />;
   }
-  if (role === t('SideBar.Mentor')) {
+  if (role === 'mentor') {
     return <RoleCard role={mentor} />;
   }
   return <RoleCard role={student} />;
