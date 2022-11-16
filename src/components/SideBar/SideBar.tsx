18d895c48f5cfc9a519ff17120e6dd7edd040191
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-16 21:11:01 +0600

    fix:fixed i18n sidebar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useAppSelector } from '../../hooks/hook';
 import { admin, student, mentor } from '../../constants/sideBar';
 import RoleCard from './RoleCards';
 
 const SideBar: React.FC = () => {
+  const { t } = useTranslation();
   const { data } = useAppSelector((state) => state.auth);
   const role: string = data?.role || '';
 
-  if (role === 'admin') {
+  if (role === t('SideBar.Admin')) {
     return <RoleCard role={admin} />;
   }
-  if (role === 'mentor') {
+  if (role === t('SideBar.Mentor')) {
     return <RoleCard role={mentor} />;
   }
   return <RoleCard role={student} />;
