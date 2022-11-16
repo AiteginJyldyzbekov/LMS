0ca690d33b3b48d55234b5ab5f7c5255617a17a5
Adilxxan <adilhanimerov82@gmail.com>
2022-11-16 14:49:56 +0600

    feat: add day data

--- src/pages/groupPage/daysItem/DaysItem.tsx
+++ src/pages/groupPage/daysItem/DaysItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { IDays } from '../../../types/types';
 import useStyles from './DaysItem.styles';
 
 const DaysItem: React.FC<IDays> = ({ id, name }) => {
   const css = useStyles();
+  const { t } = useTranslation();
   const [date, setDate] = React.useState<boolean>(false);
   const handleCollapse = () => {
     setDate(!date);
@@ -15,7 +17,7 @@ const DaysItem: React.FC<IDays> = ({ id, name }) => {
         onClick={handleCollapse}
         className={date ? css.circleAc : css.circle}
       >
-        {name}
+        {t(name)}
       </button>
     </div>
   );
