4e42c0d3fd935b47a37b4b297f08327451d3e29f
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-17 00:02:14 +0600

    fix: changed role in sideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { useAppSelector } from '../../hooks/hook';
 import { admin, student, mentor } from '../../constants/sideBar';
 import RoleCard from './RoleCards';
+import { UserRole } from '../../types/types';
 
 const SideBar: React.FC = () => {
   const { data } = useAppSelector((state) => state.auth);
   const role: string = data?.role || '';
 
-  if (role === 'admin') {
+  if (role === UserRole.admin) {
     return <RoleCard role={admin} />;
   }
-  if (role === 'mentor') {
+  if (role === UserRole.mentor) {
     return <RoleCard role={mentor} />;
   }
   return <RoleCard role={student} />;
