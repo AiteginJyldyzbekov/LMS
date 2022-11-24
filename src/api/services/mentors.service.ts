91093849774573d46850ed07685e72538f2029fd
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-24 16:24:49 +0600

    fix:fixed eslint issues

--- src/api/services/mentors.service.ts
+++ src/api/services/mentors.service.ts
@@ -4,10 +4,10 @@ import { MentorType } from '../../types/index.dto';
 import { http } from '../http';
 
 class MentorService {
-    async getAll() {
-        const responce: AxiosResponse<[MentorType]> = await http.get('mentors');
-        return responce.data;
-    }
+  async getAll() {
+    const responce: AxiosResponse<[MentorType]> = await http.get('mentors');
+    return responce.data;
+  }
 }
 
 export default MentorService;
