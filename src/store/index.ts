1aa0534df39cca5085da3c36173819b212eb24a7
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-11-26 17:11:29 +0600

    fix: changed slice name

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,14 +3,14 @@ import studentSlice from './slices/AllStudentsSlice';
 import AuthSlice from './slices/AuthSlice';
 import GroupSlice from './slices/GroupSlice';
 import rootSlice from './slices/RootSlice';
-import MentorsSlice from './slices/MentorSlice';
+import MentorSlice from './slices/MentorSlice';
 
 const store = configureStore({
   reducer: {
     root: rootSlice,
     auth: AuthSlice,
     group: GroupSlice,
-    mentors: MentorsSlice,
+    mentor: MentorSlice,
     student: studentSlice,
   },
 });
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -2,8 +2,7 @@ import { useAppSelector } from '../hooks/hook';
 
 export const useSelectorGroups = () => useAppSelector((state) => state.group);
 
-export const useSelectorMentors = () =>
-  useAppSelector((state) => state.mentors);
+export const useSelectorMentors = () => useAppSelector((state) => state.mentor);
 
 export const useSelectorAllStudents = () =>
   useAppSelector((state) => state.student);
