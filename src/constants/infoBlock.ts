ec632774a7a50149e550009866af78e97bf05cf1
Kairat <asyl06kair@gmail.com>
2023-01-08 22:18:07 +0600

    fix

--- src/constants/infoBlock.ts
+++ src/constants/infoBlock.ts
@@ -4,31 +4,31 @@ export const info: IInfo[] = [
   {
     id: 1,
     title: 'infoBlock.department',
-    res: 'Front-End',
+    dataItem: 'Front-End',
   },
   {
     id: 2,
     title: 'infoBlock.firstName',
-    res: 'Ulan',
+    dataItem: 'Ulan',
   },
   {
     id: 3,
     title: 'infoBlock.lastName',
-    res: 'Sulaimanov',
+    dataItem: 'Sulaimanov',
   },
   {
     id: 4,
     title: 'infoBlock.email',
-    res: 'ulik@gmail.com',
+    dataItem: 'ulik@gmail.com',
   },
   {
     id: 5,
     title: 'infoBlock.phoneNumber',
-    res: '+996557120820',
+    dataItem: '+996557120820',
   },
   {
     id: 6,
     title: 'infoBlock.status',
-    res: 'active',
+    dataItem: 'active',
   },
 ];
