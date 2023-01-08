d81e263d11d06141be8de245b6e22c9a06ffddc2
Kairat <asyl06kair@gmail.com>
2023-01-08 21:02:18 +0600

    feat: mentor groups table

--- src/components/InfoBlock/InfoBlock.tsx
+++ src/components/InfoBlock/InfoBlock.tsx
@@ -14,14 +14,15 @@ const InfoBlock: React.FC<IProps> = ({ info }) => {
       <Grid
         item
         xl={8}
-        lg={10}
-        xs={10}
-        md={10}
+        lg={12}
+        xs={12}
+        md={12}
         sx={{
           display: 'flex',
           flexWrap: 'wrap',
-          gap: '15px',
+          gap: '25px',
           marginTop: '20px',
+          marginBottom: '20px',
         }}
       >
         {info.map((item) => (
@@ -29,7 +30,7 @@ const InfoBlock: React.FC<IProps> = ({ info }) => {
             <Typography component="p">{t(item.title)}:</Typography>
             <Grid
               sx={{
-                width: '300px',
+                width: '400px',
                 height: '40px',
                 border: '1px solid rgba(224, 224, 224, 1)',
                 borderRadius: '3px',
--- src/pages/ADMIN/mentor/MentorDataPage.tsx
+++ src/pages/ADMIN/mentor/MentorDataPage.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 import InfoBlock from '../../../components/InfoBlock/InfoBlock';
 import { info } from '../../../constants/infoBlock';
 import PageContainer from '../../../components/PageContainer/PageContainer';
+import GroupTable from '../../../components/Tables/GroupTable';
+import { useSelectorGroups } from '../../../store/selectors';
+import TableContainer from '../../../components/TableContainer/TableContainer';
+import { getAllGroups } from '../../../store/slices/GroupSlice';
+import { useAppDispatch } from '../../../hooks/hook';
 
-const MentorDataPage = () => {
+const MentorDataPage: React.FC = () => {
   const { t } = useTranslation();
+  const { result, loading } = useSelectorGroups();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAllGroups());
+  }, []);
+
+  const renderList = useMemo(
+    () => result.map((row) => <GroupTable key={row.id} {...row} />),
+    [result]
+  );
+
   return (
     <PageContainer
-      name={t('infoBlock.btnText')}
+      name={t('infoBlock.title')}
       btnText={t('infoBlock.btnText')}
       puth="/editMentor/1"
     >
       <InfoBlock info={info} />
+      <TableContainer
+        isLoading={loading}
+        isFilter
+        Header={
+          <TableRow>
+            <TableCell>{t('Groups.id')}</TableCell>
+            <TableCell>{t('Groups.department')}</TableCell>
+            <TableCell>{t('Groups.name')}</TableCell>
+            <TableCell align="right">{t('Groups.startDate')}</TableCell>
+            <TableCell align="right">{t('Groups.amount')}</TableCell>
+            <TableCell align="right">{t('Groups.duration')}</TableCell>
+            <TableCell align="right">{t('Groups.status')}</TableCell>
+          </TableRow>
+        }
+        Body={renderList}
+      />
     </PageContainer>
   );
 };
