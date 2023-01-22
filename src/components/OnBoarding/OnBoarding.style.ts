0f323f99b6869a3d8cb1c6449e75a729abc7b384
aiskairr <aminhansss2222@gmail.com>
2023-01-22 17:33:20 +0600

    finish onBoarding Slider

--- src/components/OnBoarding/OnBoarding.style.ts
+++ src/components/OnBoarding/OnBoarding.style.ts
@@ -1,13 +1,36 @@
+import { WidthFull } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles(() => ({
   onBoarding: {
     gridArea: 'o',
+    "& h4" : {
+      position: "relative",
+      "&::after": {
+        content: `''`,
+        position: "absolute",
+        height: "1px",
+        backgroundColor: "#515151",
+        width: "70%",
+        top: "20px",
+        left: "15%"
+      }
+    }
   },
   wrapper: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
-    gap: '40px',
   },
+  "@global": {
+    '.slick-disabled': {
+      backgroundColor: "#D9D9D966 !important",
+    },
+    '.slick-next:before' : {
+      content: `" "`
+    },
+    '.slick-prev:before' : {
+      content: `" "`
+    },
+  }
 }));
--- src/components/OnBoarding/OnBoarding.tsx
+++ src/components/OnBoarding/OnBoarding.tsx
@@ -2,19 +2,81 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import OnboardingCard from '../OnboardingCard/OnboardingCard';
 import { useStyles } from './OnBoarding.style';
+import Slider from 'react-slick';
+import Arrow from '../../../public/images/onBoardingSlider/Arrow.svg';
+import SecondArrow from '../../../public/images/onBoardingSlider/SecondArrow.svg';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        position: 'absolute',
+        right: '60px',
+        top: '-45px',
+        width: '40px',
+        height: '40px',
+        background: '#D9D9D999',
+        borderRadius: '34px',
+        backgroundImage: `url(${Arrow})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        position: 'absolute',
+        left: '89%',
+        top: '-45px',
+        width: '40px',
+        height: '40px',
+        background: '#D9D9D999',
+        borderRadius: '34px',
+        backgroundImage: `url(${SecondArrow})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+      onClick={onClick}
+    />
+  );
+}
 
 const OnBoarding = () => {
   const styles = useStyles();
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
   return (
     <div className={styles.onBoarding}>
       <Typography variant="h4">Onboarding</Typography>
       <br />
-      <div className={styles.wrapper}>
+      <Slider className={styles.wrapper} {...settings}>
         <OnboardingCard />
         <OnboardingCard />
         <OnboardingCard />
         <OnboardingCard />
-      </div>
+      </Slider>
     </div>
   );
 };
