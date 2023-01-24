debae9a4a2ee311c4b8621d44bd0528efcdaed7b
Azim-941223 <urmanbetovazim@gmail.com>
2023-01-25 00:40:12 +0600

    fix/styled,install another svg

--- src/components/NavbarStudent/Navbar.style.ts
+++ src/components/NavbarStudent/Navbar.style.ts
@@ -3,35 +3,43 @@ import { makeStyles } from '@mui/styles';
 export const useStyles = makeStyles(() => ({
   navbar_students: {
     borderRight: '1px solid #616161',
-    height: '100%',
+    height: '100vh',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
   logo: {
-    paddingTop: '20px',
+    width:'100%',
+    height:'16%',
+    textAlign:'center',
+    paddingTop:'30%'
   },
   navbar: {
+    width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    paddingTop: '25px',
-    height: '70%',
+    alignItems:'center',
+    justifyContent:'space-between',
+    height: '64%',
   },
   path: {
-    width: '65px',
-    height: '65px',
-    textAlign: 'center',
+    width: '60%',
+    height: '15%',
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    paddingTop:'10%',
     textDecoration: 'none',
     color: 'inherit',
     backgroundColor: 'transparent',
-    padding: '10% 2%',
-    margin: '20px 0 0 0',
   },
   icon: {
+    width:'50%',
     filter:
       'invert(56%) sepia(0%) saturate(10%) hue-rotate(219deg) brightness(97%) contrast(91%)',
   },
   icon_activ: {
+    width:'50%',
     filter:
       'invert(100%) sepia(1%) saturate(7498%) hue-rotate(58deg) brightness(116%) contrast(101%)',
   },
@@ -48,13 +56,11 @@ export const useStyles = makeStyles(() => ({
     color: '#fff',
   },
   active: {
-    width: '65px',
-    height: '65px',
+    width: '60%',
+    height: '15%',
     borderRadius: '12px',
     backgroundColor: 'transparent',
-    padding: '10% 2%',
-    margin: '20px 0 0 0',
     cursor: 'pointer',
     background: 'linear-gradient(135deg, #BD4E1B 0%, #FD04EC 100%)',
-  },
+  }
 }));
