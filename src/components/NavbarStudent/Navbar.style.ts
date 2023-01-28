3ed654735fc2eaa881f80ff5465f330c6b717aa8
Azim-941223 <urmanbetovazim@gmail.com>
2023-01-28 17:22:07 +0600

    fix: layout navbar

--- src/components/NavbarStudent/Navbar.style.ts
+++ src/components/NavbarStudent/Navbar.style.ts
@@ -13,7 +13,6 @@ export const useStyles = makeStyles(() => ({
     height: '12%',
     textAlign: 'center',
     paddingTop: '15%',
-    border: '1px solid green',
     '@media (max-width: 1500px)': {
       height: '13%',
     },
@@ -34,9 +33,8 @@ export const useStyles = makeStyles(() => ({
     alignItems: 'center',
     justifyContent: 'space-between',
     height: '58%',
-    border: '1px solid red',
     '@media (max-width: 1500px)': {
-      height: '57%',
+      height: '70%',
     },
   },
   path: {
@@ -47,12 +45,26 @@ export const useStyles = makeStyles(() => ({
     alignItems: 'center',
     textDecoration: 'none',
     color: 'inherit',
-    paddingTop: '3%',
     backgroundColor: 'transparent',
     '@media (max-width: 1500px)': {
       height: '15%',
-      width: '58%',
-      padding: '0',
+      width: '60%',
+    },
+    '@media (max-width: 1300px)': {
+      height: '13%',
+      width: '60%',
+    },
+  },
+  active: {
+    width: '60%',
+    height: '15%',
+    borderRadius: '12px',
+    backgroundColor: 'transparent',
+    cursor: 'pointer',
+    background: 'linear-gradient(135deg, #BD4E1B 0%, #FD04EC 100%)',
+    '@media (max-width: 1500px)': {
+      height: '15%',
+      width: '60%',
     },
     '@media (max-width: 1300px)': {
       height: '13%',
@@ -66,7 +78,7 @@ export const useStyles = makeStyles(() => ({
       'invert(56%) sepia(0%) saturate(10%) hue-rotate(219deg) brightness(97%) contrast(91%)',
     '@media (max-width: 1500px)': {
       width: '40%',
-      paddingTop: '8%',
+      paddingTop: '12%',
     },
   },
   icon_activ: {
@@ -76,7 +88,7 @@ export const useStyles = makeStyles(() => ({
       'invert(100%) sepia(1%) saturate(7498%) hue-rotate(58deg) brightness(116%) contrast(101%)',
     '@media (max-width: 1500px)': {
       width: '40%',
-      paddingTop: '8%',
+      paddingTop: '12%',
     },
   },
   title: {
@@ -97,22 +109,6 @@ export const useStyles = makeStyles(() => ({
     paddingTop: '10%',
     '@media (max-width: 1500px)': {
       fontSize: '12px',
-    },
-  },
-  active: {
-    width: '60%',
-    height: '15%',
-    borderRadius: '12px',
-    backgroundColor: 'transparent',
-    cursor: 'pointer',
-    background: 'linear-gradient(135deg, #BD4E1B 0%, #FD04EC 100%)',
-    '@media (max-width: 1500px)': {
-      height: '16%',
-      width: '60%',
-    },
-    '@media (max-width: 1300px)': {
-      height: '13%',
-      width: '60%',
     },
   },
 }));
