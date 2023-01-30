3e14541021ff52b5902d281890e31bfe11c9ecb6
Azim-941223 <urmanbetovazim@gmail.com>
2023-01-30 22:25:29 +0600

    fix:layout

--- src/components/NavbarStudent/Navbar.style.ts
+++ src/components/NavbarStudent/Navbar.style.ts
@@ -31,47 +31,67 @@ export const useStyles = makeStyles(() => ({
     },
   },
   path: {
-    width: '60px',
-    height: '60px',
+    width: '75px',
+    height: '75px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     textDecoration: 'none',
     color: 'inherit',
     backgroundColor: 'transparent',
+    '@media (max-width: 1500px)': {
+      width: '60px',
+      height: '60px',
+    },
   },
   active: {
-    width: '60px',
-    height: '60px',
+    width: '75px',
+    height: '75px',
     borderRadius: '12px',
     backgroundColor: 'transparent',
     background: 'linear-gradient(135deg, #BD4E1B 0%, #FD04EC 100%)',
     boxShadow: '0px 0px 4px #B9208C',
+    '@media (max-width: 1500px)': {
+      width: '60px',
+      height: '60px',
+    },
   },
   icon: {
-    width: '40%',
+    width: '50%',
     paddingTop: '8px',
     filter:
       'invert(56%) sepia(0%) saturate(10%) hue-rotate(219deg) brightness(97%) contrast(91%)',
+    '@media (max-width: 1500px)': {
+      width: '40%',
+    },
   },
   icon_activ: {
-    width: '40%',
+    width: '50%',
     paddingTop: '8px',
     filter:
       'invert(100%) sepia(1%) saturate(7498%) hue-rotate(58deg) brightness(116%) contrast(101%)',
+    '@media (max-width: 1500px)': {
+      width: '40%',
+    },
   },
   title: {
     fontWeight: '300',
-    fontSize: '12px',
+    fontSize: '14px',
     lineHeight: '14px',
     color: '#898989',
     paddingTop: '7px',
+    '@media (max-width: 1500px)': {
+      fontSize: '12px',
+    },
   },
   title_activ: {
     fontWeight: '300',
-    fontSize: '12px',
+    fontSize: '14px',
     lineHeight: '14px',
     color: '#fff',
     paddingTop: '7px',
+    '@media (max-width: 1500px)': {
+      fontSize: '12px',
+    },
   },
 }));
