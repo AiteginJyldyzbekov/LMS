3e33bbeb599d35e262f95ff8a65b5bc693c30b7b
aiskairr <aminhansss2222@gmail.com>
2023-02-10 15:53:16 +0600

    feat/ layout calendar only mui

--- src/components/Calendar/Calendar.style.ts
+++ src/components/Calendar/Calendar.style.ts
@@ -7,4 +7,150 @@ export const useStyles = makeStyles(() => ({
     borderRadius: '12px',
     gridArea: 'c',
   },
+  $M: {
+    color: 'red',
+  },
+  '@global': {
+    '.MuiCalendarPicker-root': {
+      width: '100% !important',
+      minHeight: '100% !important',
+    },
+    '.MuiDayPicker-header ': {
+      justifyContent: 'space-around !important',
+      '& span': {
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '19px',
+        display: 'flex',
+        justifyContent: 'center',
+        width: '36px',
+        height: '36px',
+      },
+    },
+    '.MuiDayPicker-weekContainer': {
+      justifyContent: 'space-around !important',
+
+      '& button': {
+        width: '36px',
+        height: '36px',
+        display: 'flex',
+        justifyContent: 'center',
+        fontStyle: 'normal',
+        fontWeight: 500,
+        fontSize: '16px',
+        lineHeight: '19px',
+        transition: 'ease-in-out .3s',
+        alignItems: 'center',
+        border: 'none',
+        background: 'none',
+        cursor: 'pointer',
+        color: "white",
+        '&:nth-last-child(1)': {
+          color: '#FF5F5F',
+        },
+        '&:nth-last-child(-n+2)': {
+          color: '#FF5F5F',
+        },
+      },
+    },
+    '.MuiDayPicker-monthContainer': {
+      minHeight: '344.99px !important',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+    },
+    '.MuiDayPicker-weekDayLabel': {
+      color: 'white !important',
+      '&:nth-last-child(1)': {
+        color: '#FF5F5F',
+      },
+      '&:nth-last-child(-n+2)': {
+        color: '#FF5F5F',
+      },
+    },
+    '.css-dxmyjx-MuiButtonBase-root-MuiPickersDay-root.Mui-selected': {
+      background: '#6B9AFF !important',
+      borderRadius: '7px !important',
+      '&:nth-last-child(1)': {
+        color: 'white !important',
+        background: '#FF00F5 !important',
+        borderRadius: '7px !important',
+      },
+      '&:nth-last-child(-n+2)': {
+        color: 'white !important',
+        background: '#FF00F5 !important',
+        borderRadius: '7px !important',
+      },
+    },
+    '.css-sf5t6v-PrivatePickersSlideTransition-root-MuiDayPicker-slideTransition':
+      {
+        overflowX: 'visible !important',
+      },
+    '.css-dxmyjx-MuiButtonBase-root-MuiPickersDay-root': {
+      backgroundColor: '#2C2E34 !important',
+    },
+    '.css-1d1d7bu-MuiButtonBase-root-MuiPickersDay-root.Mui-selected': {
+      background: '#6B9AFF !important',
+      borderRadius: '7px !important',
+      '&:nth-last-child(1)': {
+        color: 'white !important',
+        background: '#FF00F5 !important',
+        borderRadius: '7px !important',
+      },
+      '&:nth-last-child(-n+2)': {
+        color: 'white !important',
+        background: '#FF00F5 !important',
+        borderRadius: '7px !important',
+      },
+    },
+    '.css-1d1d7bu-MuiButtonBase-root-MuiPickersDay-root:not(.Mui-selected)': {
+      border: 'none !important',
+    },
+    '.css-1d1d7bu-MuiButtonBase-root-MuiPickersDay-root': {
+      background: 'none !important',
+    },
+    '.css-i4bv87-MuiSvgIcon-root': {
+      color: 'white !important',
+      width: '100% !important',
+      display: 'flex !important',
+    },
+    '.css-1tkx1wf-MuiSvgIcon-root-MuiPickersCalendarHeader-switchViewIcon': {
+      display: 'none !important',
+    },
+    '.css-9reuh9-MuiPickersArrowSwitcher-root ': {
+      width: '100% !important',
+      display: 'flex !important',
+      justifyContent: 'space-between !important',
+    },
+    '.css-10fao8b-MuiPickersCalendarHeader-labelContainer': {
+      position: 'absolute !important',
+      margin: '0',
+      top: '50%',
+      left: '50%',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+    '.css-nk89i7-MuiPickersCalendarHeader-root': {
+      position: 'relative !important',
+    },
+    '.css-dplwbx-MuiPickersCalendarHeader-label': {
+      marginRight: '0 !important',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '20px',
+      lineHeight: '23px',
+      textAlign: 'center',
+      color: '#FFFFFF',
+    },
+    ".css-1dk40rm-MuiButtonBase-root-MuiPickersDay-root": {
+      color: "#565656 !important",
+      '&:nth-last-child(1)': {
+        color: '#565656 !important',
+      },
+      '&:nth-last-child(-n+2)': {
+        color: '#565656 !important',
+      },
+    }
+  },
 }));
--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import { useStyles } from './Calendar.style';
+import dayjs, { Dayjs } from 'dayjs';
+import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/ru';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { deDE } from '@mui/x-date-pickers';
+import Grid from '@mui/material/Grid';
 
 const Calendar = () => {
   const styles = useStyles();
-  return <div className={styles.calendar_card}>Calendar</div>;
+  const [date, setDate] = React.useState<Dayjs | null>(dayjs());
+  const theme = createTheme(
+    {
+      palette: {
+        primary: { main: '#1976d2' },
+      },
+    },
+    deDE
+  );
+  return (
+    <div className={styles.calendar_card}>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+          <Grid
+            item
+            sx={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'start',
+            }}
+          >
+            <CalendarPicker
+              date={date}
+              onChange={(newDate) => setDate(newDate)}
+              showDaysOutsideCurrentMonth={true}
+            />
+          </Grid>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default Calendar;
