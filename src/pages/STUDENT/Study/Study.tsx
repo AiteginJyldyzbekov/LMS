0b015344200662ab84364d9b0c5c2cf7ea8dcee6
Azim-941223 <urmanbetovazim@gmail.com>
2023-02-17 01:14:22 +0600

    fix:CI

--- src/pages/STUDENT/Study/Study.tsx
+++ src/pages/STUDENT/Study/Study.tsx
@@ -2,13 +2,11 @@ import React, { FC } from 'react';
 import StudentHeader from '../../../components/studentHeader/studentHeader';
 import RoutesStudy from '../../../components/StudentStudy/Routes/RoutesStudy';
 
-const Study: FC = () => {
-  return (
-    <div>
-      <StudentHeader />
-      <RoutesStudy />
-    </div>
-  );
-};
+const Study: FC = () => (
+  <div>
+    <StudentHeader />
+    <RoutesStudy />
+  </div>
+);
 
 export default Study;
